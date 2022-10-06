9734259813a9f85188fa34643ad37a2a060d7037
gilmore606 <dan.burford@gmail.com>
2022-10-06 01:22:59 -0700

    numpad

--- src/main/kotlin/input/KeyboardProcessor.kt
+++ src/main/kotlin/input/KeyboardProcessor.kt
@@ -3,19 +3,20 @@ package input
 import com.badlogic.gdx.Input
 import ktx.app.KtxInputAdapter
 import render.GameScreen
-import util.EAST
-import util.NORTH
-import util.SOUTH
-import util.WEST
+import util.*
 
 object KeyboardProcessor : KtxInputAdapter {
 
     override fun keyDown(keycode: Int): Boolean {
         when (keycode) {
-            Input.Keys.W -> { GameScreen.movePlayer(NORTH) }
-            Input.Keys.A -> { GameScreen.movePlayer(WEST) }
-            Input.Keys.S -> { GameScreen.movePlayer(SOUTH) }
-            Input.Keys.D -> { GameScreen.movePlayer(EAST) }
+            Input.Keys.NUMPAD_8 -> { GameScreen.movePlayer(NORTH) }
+            Input.Keys.NUMPAD_7 -> { GameScreen.movePlayer(NORTHWEST) }
+            Input.Keys.NUMPAD_4 -> { GameScreen.movePlayer(WEST) }
+            Input.Keys.NUMPAD_1 -> { GameScreen.movePlayer(SOUTHWEST) }
+            Input.Keys.NUMPAD_2 -> { GameScreen.movePlayer(SOUTH) }
+            Input.Keys.NUMPAD_3 -> { GameScreen.movePlayer(SOUTHEAST) }
+            Input.Keys.NUMPAD_6 -> { GameScreen.movePlayer(EAST) }
+            Input.Keys.NUMPAD_9 -> { GameScreen.movePlayer(NORTHEAST) }
 
             else -> return super.keyDown(keycode)
         }
