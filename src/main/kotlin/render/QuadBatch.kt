60d3b9a882bc3087789af836de2e4e784f036d9b
gilmore606 <dan.burford@gmail.com>
2022-10-17 14:57:03 -0700

    bugfix

--- src/main/kotlin/render/QuadBatch.kt
+++ src/main/kotlin/render/QuadBatch.kt
@@ -86,10 +86,10 @@ class QuadBatch(
     private fun addQuad(ix0: Double, iy0: Double, ix1: Double, iy1: Double,
                         textureIndex: Int, lightR: Float = 1f, lightG: Float = 1f, lightB: Float = 1f,
                         grayOut: Float = 0f) {
-        val x0 = ix0.toFloat() - GameScreen.scrollX * GameScreen.zoom.toFloat()
-        val y0 = -iy0.toFloat() + GameScreen.scrollY * GameScreen.zoom.toFloat()
-        val x1 = ix1.toFloat() - GameScreen.scrollX * GameScreen.zoom.toFloat()
-        val y1 = -iy1.toFloat() + GameScreen.scrollY * GameScreen.zoom.toFloat()
+        val x0 = ix0.toFloat() - if (isScrolling) GameScreen.scrollX * GameScreen.zoom.toFloat() else 0f
+        val y0 = -iy0.toFloat() + if (isScrolling) GameScreen.scrollY * GameScreen.zoom.toFloat() else 0f
+        val x1 = ix1.toFloat() - if (isScrolling) GameScreen.scrollX * GameScreen.zoom.toFloat() else 0f
+        val y1 = -iy1.toFloat() + if (isScrolling) GameScreen.scrollY * GameScreen.zoom.toFloat() else 0f
         val tx0 = ((textureIndex % tileSet.tilesPerRow) * tileSet.tileRowStride).toFloat() + (tileSet.tilesPerRow * tilePad)
         val ty0 = ((textureIndex / tileSet.tilesPerRow) * tileSet.tileColumnStride).toFloat() + (tileSet.tilesPerColumn * tilePad)
         val tx1 = (((textureIndex % tileSet.tilesPerRow) + 1) * tileSet.tileRowStride).toFloat() - (tileSet.tilesPerRow * tilePad)
