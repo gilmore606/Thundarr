517ee3377d880b72247816e0b9ee56759d95fb4f
gilmore606 <dan.burford@gmail.com>
2023-02-28 10:20:35 -0800

    tavern name and skel

---
+++ src/main/kotlin/world/gen/features/Tavern.kt
@@ -0,0 +1,25 @@
+package world.gen.features
+
+import kotlinx.serialization.Serializable
+import util.XY
+import world.ChunkScratch
+import world.gen.biomes.Glacier
+import world.gen.biomes.Ocean
+
+@Serializable
+class Tavern(
+    private val name: String,
+    private val villageDirection: XY
+) : ChunkFeature(
+    4, Stage.BUILD
+) {
+
+    companion object {
+        fun canBuildOn(meta: ChunkScratch) = meta.biome !in listOf(Ocean, Glacier)
+    }
+
+    override fun doDig() {
+
+    }
+
+}
