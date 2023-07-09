7201551837b723c1409eecafa950389046be04fb
gilmore606 <dan.burford@gmail.com>
2023-07-08 18:18:54 -0700

    fix GL visual bug with mirror glyphs

--- src/main/kotlin/render/batches/QuadBatch.kt
+++ src/main/kotlin/render/batches/QuadBatch.kt
@@ -88,8 +88,8 @@ class QuadBatch(
         val lightG = min(visibility, light.g + App.level.weather.lightning.g) * Screen.brightness
         val lightB = min(visibility, light.b + App.level.weather.lightning.b) * Screen.brightness
         val grayOut = grayBlend ?: if (visibility < 1f) 1f else App.level.weather.lightning.r * 0.6f
-        val itx0 = if (mirror) 1f else 0f
-        val itx1 = if (mirror) 0f else 1f
+        val itx0 = if (mirror) 0.98f else 0f
+        val itx1 = if (mirror) 0.02f else 1f
         addQuad(x0, y0, x1, y1, itx0, 0f, itx1, if (isTall) 2f else 1f, textureIndex, lightR, lightG, lightB, alpha, grayOut, hue, rotate, waves)
     }
 
